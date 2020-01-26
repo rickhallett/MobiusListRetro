13406e60f08ea4e4b3ddf1e73633b4c85663498c
Rick Hallett <rickhallett@live.co.uk>
2020-01-26 18:56:22 +0000

    2.2

--- Startup.cs
+++ Startup.cs
@@ -41,7 +41,7 @@ namespace MobiusList2
             }
             else
             {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                // The default HSTS vlue is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             
