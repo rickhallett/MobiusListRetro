99203415a71cb558cfeec6313bc7a13182ae7a56
Rick Hallett <rickhallett@live.co.uk>
2020-01-26 18:46:55 +0000

    azurelin add explicit ssl port

--- Program.cs
+++ Program.cs
@@ -19,6 +19,7 @@ namespace MobiusList2
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+//                .UseSetting("https_port", "5001")
                 .UseStartup<Startup>();
     }
 }
